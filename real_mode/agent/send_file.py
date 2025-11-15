da17aca1a96c397f2f0af8c5d339171a8d39f129
AnikethGorty <86899551+AnikethGorty@users.noreply.github.com>
2025-11-15 22:20:50 +0530

    wdef

--- real_mode/agent/send_file.py
+++ real_mode/agent/send_file.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import os
 
 if len(sys.argv) < 3:
     print("Usage: python send_file.py <peer-ip> <file-path>")
@@ -8,10 +9,15 @@ if len(sys.argv) < 3:
 peer = sys.argv[1]
 file_path = sys.argv[2]
 
+if not os.path.exists(file_path):
+    print("File not found:", file_path)
+    sys.exit(1)
+
 url = f"http://{peer}:5000/upload"
 
 with open(file_path, "rb") as f:
     files = {"file": f}
+    print("[SENDER] Sending file…")
     r = requests.post(url, files=files)
 
-print("Response:", r.text)
+print("[SENDER] Response:", r.text)
